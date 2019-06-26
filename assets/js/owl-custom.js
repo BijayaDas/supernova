55c3b4b0e57d3bc8df291cc27ff4905d3b1c4d91
BIJAYA KRISHNA <bkdthenovelist@gmail.com>
2019-06-27 00:25:11 +0530

    Initial commit

---
+++ assets/js/owl-custom.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+   
+    /* ======= Owl Carousel ======= */    
+    /* Ref: http://owlgraphic.com/owlcarousel/index.html */
+    
+    $("#home-slideshow").owlCarousel({
+        autoplay:true,       
+        autoPlayTimeout : 6000,
+        smartSpeed : 400,
+        autoplayHoverPause:true,
+        items: 1,
+        loop: true
+        
+    });
+
+    $("#home-slideshow1").owlCarousel({
+        autoplay:true,       
+        autoPlayTimeout : 6000,
+        smartSpeed : 400,
+        autoplayHoverPause:true,
+        items: 1,
+        loop: true
+        
+    });
+
+    $("#home-slideshow2").owlCarousel({
+        autoplay:true,       
+        autoPlayTimeout : 6000,
+        smartSpeed : 400,
+        autoplayHoverPause:true,
+        items: 1,
+        loop: true
+        
+    });
+
+
+    $("#press-slideshow").owlCarousel({      
+        autoplay:true,       
+        autoPlayTimeout : 6000,
+        loop: true,
+        smartSpeed : 400,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            900:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+                
+    });
+    
+    $("#jobs-promo-slideshow").owlCarousel({
+        
+        nav: true, // Show next and prev buttons
+        //navText: false,
+        items : 1,
+        autoplay:true,       
+        autoPlayTimeout : 6000,
+        loop: true,
+        smartSpeed : 400,
+        autoHeight : true,
+        loop: true
+        
+    });
+
+});
